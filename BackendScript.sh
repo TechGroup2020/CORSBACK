e422a632607f1b3fce74ef57709ed4a0184cac81
TechGroup2020 <65551463+TechGroup2020@users.noreply.github.com>
2020-12-25 09:28:23 -0500

    Add files via upload

---
+++ BackendScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker stop tochekcont2
+docker rm tochekcont2
+docker build -t testimg2 .
+docker run -i --name tochekcont2 -p 6060:6060 testimg2  /bin/bash
+docker run -it --name tochekcont2 testimg2  /bin/bash
+docker start tochekcont2
+docker exec tochekcont2 ./startup.sh
