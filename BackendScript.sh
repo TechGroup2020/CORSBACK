c26a5392e037efc72166721fbbf42b4222240f5c
TechGroup2020 <65551463+TechGroup2020@users.noreply.github.com>
2021-04-27 00:33:51 +0300

    Update BackendScript.sh

--- BackendScript.sh
+++ BackendScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t testimg2 .
-docker run -i --name tochekcont2 -p 6060:6060 testimg2  /bin/bash
-docker run -it --name tochekcont2 testimg2  /bin/bash
+docker run -i --name tochekcont2 -p 6060:6060 testimg2 /bin/bash
+docker run -it --name tochekcont2 testimg2 /bin/bash
 docker start tochekcont2
 docker exec tochekcont2 ./startup.sh
