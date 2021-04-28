1303d8baedcb6474f9bb911e4409de6b5939ebcd
TechGroup2020 <65551463+TechGroup2020@users.noreply.github.com>
2021-04-28 22:09:05 +0300

    Update BackendScript.sh

--- BackendScript.sh
+++ BackendScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker build -t testimg2 .
-docker run -i --name tochekcont2 --network=gomathanetwork --ip 192.168.10.3 -p 6060:6060 testimg2 /bin/bash
-docker run -it --name tochekcont2 testimg2 /bin/bash
-docker start tochekcont2
-docker exec tochekcont2 ./startup.sh
+docker build -t Backendimg .
+docker run -i --name Backendcont --network=gomathanetwork --ip 192.168.10.3 -p 6060:6060 Backendimg /bin/bash
+docker run -it --name Backendcont Backendimg /bin/bash
+docker start Backendcont
+docker exec Backendcont ./startup.sh
